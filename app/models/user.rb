77f9352646a50b076f35c30e0253dc91a8891b83
Joann Lai <lai.joann.cy@gmail.com>
2017-04-17 16:24:03 +0000

    Rspec added: user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  validates :first_name, :last_name, :email, :password,:password_confirmation, presence: true
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
   has_secure_password
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'Validations' do
+    before do
+      @testuser = User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@doe.com', password: 'JaneDoe', password_confirmation: 'JaneDoe')
+      @failpassword = User.create(last_name: 'No', email: 'jane@DOE.COM', password: 'LULNo', password_confirmation: 'JaneNo')
+    end
+    it "should create users" do
+      expect(@testuser).to be_a User
+    end
+    it "should validate users" do
+      expect(@testuser).to be_valid
+      expect(@failpassword).to_not be_valid
+    end
+    it "should have same password and password confirmation" do
+      expect(@testuser.password).to eq(@testuser.password_confirmation)
+      expect(@failpassword.password).to_not eq(@failpassword.password_confirmation)
+    end
+    it "should validate presence of name" do
+      expect(@testuser.first_name).to be_present
+    end
+    it "should validate presence of last_name" do
+      expect(@testuser.last_name).to be_present
+    end
+    it "should validate presence of email" do
+      expect(@testuser.email).to be_present
+    end
+    it "should have valid password of min 6 chars" do
+      expect(@failpassword.password.length).to_not be >= 6
+      expect(@testuser.password.length).to be >= 6
+    end
+  end
+end
